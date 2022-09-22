8de16430788464af70bc7d398f4290396c85680c
João Vitor Corrêa <70239873+joaovitorcorrea06@users.noreply.github.com>
2022-09-22 08:11:31 -0300

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">Lojinha do João</Link>
+        <Link href="/">Brecho</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
